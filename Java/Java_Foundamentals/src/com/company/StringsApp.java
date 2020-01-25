e407452ea13092e75f3c1405180c67638bda421a
ksenia <ksenia.robakova@gmail.com>
2020-01-25 11:42:55 +0200

    Return Name with 1st capital letter and the rest as lower case

---
+++ Java/Java_Foundamentals/src/com/company/StringsApp.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class StringsApp {
+    public static void main(String[] args) {
+        //String name = "alice";
+        String names[] = new String []{"AliCe", "aLicE", "alice"};
+        for(String name: names){
+            System.out.println(capitalize(name));
+        }
+        //capitalize(name);
+
+    }//end of main method
+
+    static String capitalize(String word) {
+        String lowerCase = word.toLowerCase();
+        String upperCase = word.toUpperCase();
+        String result = upperCase.charAt(0) + lowerCase.substring(1, word.length());
+        return result;
+    }
+}
+
