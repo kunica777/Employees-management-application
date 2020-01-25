0fcdd471de989b3e69a4a297d95e17de865dc2f1
ksenia <ksenia.robakova@gmail.com>
2020-01-25 09:33:10 +0200

    Init

---
+++ Java/Java_Foundamentals/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// print all the application arguments
+        System.out.println("Hello World");
+    }
+}
